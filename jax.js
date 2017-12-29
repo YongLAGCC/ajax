b17410e88bd9f7849a4f0bf33acf770067fcd198
YongLAGCC <1291001957.yz@gmail.com>
2017-12-28 23:42:22 -0500

    merge

--- jax.js
+++ jax.js
@@ -1,44 +1,40 @@
 $(function(){
 
     var name = $('#name'); 
-    var birthday= $('#birthday'); 
+    var birthday= $('#birthday');
+  
     
     function addOrder(order) {
         $('#orders').append(
-            `<li>
+            `<li data-id=‘${order.id}’>
             <p>
             <strong>name:</strong>
-                <span class='noedit'>${order.name}</span>
-                <input class='edit name'/><br>         
-                </p>
+                <span class="noedit name">${order.name}</span>
+                <input class="edit name"/>        
+            </p>
             <p>
                 <strong> birth:</strong>
-                <span class='noedit'>${order.birthday}</span>
+                <span class="noedit birthday">${order.birthday}</span>
                 <input class='edit birthday'/> 
             </p>
             <button data-id=${order.id} class='remove'>X</button>
             <button class='editOrder noedit'>Edit</button>
             <button class='cancelEdit edit'>Cancel</button>
-            <button class='saveEdit edit'>Save</button>          
-            
+            <button class='saveEdit edit'>Save</button>                    
             </li>`)
     }
     $.ajax({
-        url: 'http://rest.learncode.academy/api/Yong1/friends',
+        url: 'http://rest.learncode.academy/api/Yongs/friends',
         type: 'get', 
-        success(data){
-            
+        success:function(data){           
             if(data!== null){
                 console.log('success', data);
                 $.each(data, function(i, info){
-                    console.log(info);
+                    //console.log(info);
                     addOrder(info);
                 })
             }
-        },          
-        error: function(){
-            alert('error get info')
-            }
+        }          
     })
     $('#add-Order').click(function(){
         var info = {
@@ -46,7 +42,7 @@ $(function(){
             birthday: birthday.val(),
         };
         $.ajax({
-            url: 'http://rest.learncode.academy/api/Yong1/friends',
+            url: 'http://rest.learncode.academy/api/Yongs/friends',
             type: 'post', 
             data: info, 
             success: function(newInfo){
@@ -65,47 +61,43 @@ $(function(){
 
         var li = $(this).closest('li');
         $.ajax({
-            url: 'http://rest.learncode.academy/api/Yong1/friends/'+$(this).attr('data-id'),
+            url: 'http://rest.learncode.academy/api/Yongs/friends/'+$(this).attr('data-id'),
             type: 'delete', 
             success: function(){
                 li.fadeOut(400,function(){
-                    li.remove(); 
-                    
+                li.remove();                     
                 })
             }
         })
     })
-   $('#orders').delegate('.editOrder', 'click',function(){
+    $('#orders').delegate('.editOrder', 'click',function(){
        var li = $(this).closest('li');
 
         li.find('input.name').val(li.find('span.name').html());
         li.find('input.birthday').val(li.find('span.birthday').html());
         li.addClass('edit'); 
         console.log(li);
-   })
-   $('#orders').delegate('.cancelEdit', 'click', function(){
-       $(this).closest('li').removeClass('edit');
-   });
-   $('#orders').delegate('.saveEdit', 'click', function(){
-        var li=$(this).closest('li');
-       
-   })
-   $("#orders").delegate('saveEdit', 'click',function(){
-       li = $(this).closest('li');
-       var order = {
+    })
+
+    $('#orders').delegate('.cancelEdit', 'click',function(){
+        $(this).closest('li').removeClass('edit');
+});
+  
+$('#orders').delegate('.saveEdit', 'click',function(){
+        var li = $(this).closest('li');
+        var info = {
         name: li.find('input.name').val(), 
         birthday: li.find('input.birthday').val(), 
-       };
-       $.ajax({
-            type: PUT, 
-            url: 'http://rest.learncode.academy/api/Yong1/friends/'+li.attr('data-id'),
-            data:order, 
-            success: function() {
+        };
+        $.ajax({
+            type: 'PUT', 
+            url: 'http://rest.learncode.academy/api/Yongs/friends/'+li.attr('data-id'),
+            data:info, 
+            success: function(newOrder) {
                 li.find('span.name').html(order.name); 
                 li.find('span.birthday').html(order.birthday); 
                 li.removeClass('edit'); 
-                console.log(order.birthday); 
-            
+                console.log(order.birthday);           
             },
            error: function(){
                alert('error saving info'); 
